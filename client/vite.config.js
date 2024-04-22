74a70c902c079e692b59b7e718415131fac71a57
eduvedras <edu.vedras@gmail.com>
2024-04-22 18:20:41 +0100

    speed up

--- client/vite.config.js
+++ client/vite.config.js
@@ -14,8 +14,7 @@ export default defineConfig({
 		port: 8080,
 		proxy: {
 			"/api": {
-				//target: "http://127.0.0.1:5000",
-				target: "http://eduvedras.pythonanywhere.com/",
+				target: "https://ivm.onrender.com/",
 				changeOrigin: true,
 				rewrite: (path) => path.replace(/^\/api/, "/api"),
 			},
